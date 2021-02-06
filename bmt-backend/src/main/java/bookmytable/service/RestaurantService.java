fb5a80afbc451b43ad40324fe10b377124ce9988
Antonia Nistor <antonianistor@hotmail.fr>
2021-02-06 10:44:08 -0500

    preventing errors

--- bmt-backend/src/main/java/bookmytable/service/RestaurantService.java
+++ bmt-backend/src/main/java/bookmytable/service/RestaurantService.java
@@ -24,6 +24,37 @@ public class RestaurantService {
 	@Transactional
 	public Restaurant createRestaurant( String name, String address, int[][] hours, RestaurantOwner owner, int estDuration, String menuLink, int price, String cuisine, String options) {
 		
+		if (price <= 0)
+			throw new IllegalArgumentException("You cannot enter a negative or zero price");
+		
+		if (price > 3)
+			throw new IllegalArgumentException("You cannot enter a price value higher than 3");
+		
+		if (price < 0)
+			throw new IllegalArgumentException("You cannot enter a negative price");
+		
+		if (name == null)
+			throw new IllegalArgumentException("You need to enter restaurant name");
+		
+		if (address == null)
+			throw new IllegalArgumentException("You need to enter restaurant address");
+		
+		if (hours == null)
+			throw new IllegalArgumentException("You need to enter restaurant opening hours");
+		
+		if (estDuration <= 0)
+			throw new IllegalArgumentException("You need to enter a valid restaurant estimated stay duration");
+		
+		if (menuLink == null)
+			throw new IllegalArgumentException("You need to add the restaurant menu");
+		
+		if (cuisine == null)
+			throw new IllegalArgumentException("You need to specify the type of cuisine of the restaurant");
+		
+		if (options == null)
+			throw new IllegalArgumentException("You need to enter specify the food options of the restaurant");
+		
+		
 		Restaurant restaurant = new Restaurant();
 		
 		restaurant.setAddress(address);
