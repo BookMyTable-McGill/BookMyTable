162f349d2ff9f2c979559186f757efe408f6581a
Viet Tran <viet.tran.gm@gmail.com>
2021-02-13 10:24:36 -0500

    added spring integration to cucumber tests

--- bmt-backend/src/test/java/bookmytable/stepdefinitions/CucumberTestsRunner.java
+++ bmt-backend/src/test/java/bookmytable/stepdefinitions/CucumberTestsRunner.java
@@ -3,10 +3,17 @@ package bookmytable.stepdefinitions;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import io.cucumber.spring.CucumberContextConfiguration;
+
+@CucumberContextConfiguration
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty",
         features = "src/test/java/bookmytable/features",
         glue = "bookmytable.stepdefinitions")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
 public class CucumberTestsRunner {
 }
