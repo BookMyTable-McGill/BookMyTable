c330034d9425352d7b59c40da32c05a2cf14b9db
Thomas <thomasrocks128@me.com>
2021-02-13 22:33:50 -0500

    Finished testing for restOwnerLogin

--- bmt-backend/src/main/java/bookmytable/service/RestaurantOwnerRegistrationService.java
+++ bmt-backend/src/main/java/bookmytable/service/RestaurantOwnerRegistrationService.java
@@ -17,21 +17,22 @@ public class RestaurantOwnerRegistrationService {
 
 	
 	@Transactional 
-	public RestaurantOwner registerRestaurantOwner(String name, String password, String email, long id) {
+	public RestaurantOwner registerRestaurantOwner(String name, String password, String email) {
 		String error = " ";
-		if(restaurantOwnerRepository.findRestaurantOwnerById(id) != null) {
-			error = "Restaurant Owner already exists.";
-			throw new IllegalArgumentException(error);
-		}else if(restaurantOwnerRepository.findRestaurantOwnerByEmail(email) != null) {
-			error = "This email is already used by another restaurant owner.";
-			throw new IllegalArgumentException(error);
-		}
+//		if(restaurantOwnerRepository.findRestaurantOwnerById(id) != null) {
+//			error = "Restaurant Owner already exists.";
+//			throw new IllegalArgumentException(error);
+//		}else if(restaurantOwnerRepository.findRestaurantOwnerByEmail(email) != null) {
+//			error = "This email is already used by another restaurant owner.";
+//			throw new IllegalArgumentException(error);
+//		}
 		
 		RestaurantOwner owner = new RestaurantOwner();
 		owner.setEmail(email);
-		owner.setId(id);
+		//owner.setId(id);
 		owner.setName(name);
 		owner.setPassword(password);
+		owner.setRestaurants(null);
 		restaurantOwnerRepository.save(owner);
 		return owner;
 	}
