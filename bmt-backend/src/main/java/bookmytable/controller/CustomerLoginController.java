57720cf99d2da2879761ab444e34b58212781823
Viet Tran <viet.tran.gm@gmail.com>
2021-02-14 13:43:44 -0500

    customer login fix

--- bmt-backend/src/main/java/bookmytable/controller/CustomerLoginController.java
+++ bmt-backend/src/main/java/bookmytable/controller/CustomerLoginController.java
@@ -3,13 +3,11 @@ package bookmytable.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bookmytable.dto.CustomerDTO;
 import bookmytable.model.Customer;
-
 import bookmytable.service.CustomerLoginService;
 ;
 
@@ -26,7 +24,7 @@ public class CustomerLoginController {
 		boolean loginCheck = false;
 		Customer customer = customerLoginService.getCustomerByEmail(email);
 		if (customer != null) {
-		 loginCheck = CustomerLoginService.loginCustomer(customer, password, email);
+		 loginCheck = customerLoginService.loginCustomer(customer, email, password);
 	
 		}
 		
