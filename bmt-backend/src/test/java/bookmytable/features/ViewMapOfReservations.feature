db393f9ebf1ad8595a1395898171b538322bea87
lu-barb <55468973+lu-barb@users.noreply.github.com>
2021-02-14 19:35:37 -0500

    final customer map tests

--- bmt-backend/src/test/java/bookmytable/features/ViewMapOfReservations.feature
+++ bmt-backend/src/test/java/bookmytable/features/ViewMapOfReservations.feature
@@ -9,17 +9,11 @@ Feature: View Map Of Reservations
     Given restaurant owner <restaurant_owner> is logged into BookMyTable as a restaurant owner
     And <restaurant_owner> owns <restaurant_id>
     And the following reservations are registered to the restaurant <restaurant_id>:
-      | reservation_id | reservation_datetime | customer_id | table_id | table_coordinates | restaurant_id |
-      | 0001           | 2021-03-23 20:34:45  | 69420       | 3246     | 0,2               | 0001          |
-      | 0002           | 2021-03-23 19:48:58  | 12234       | 3240     | 0,8               | 0001          |
-      | 0003           | 2021-03-23 19:15:25  | 82903       | 2358     | 2,6               | 0001          |
-      | 0004           | 2021-03-23 16:32:24  | 23412       | 3246     | 0,2               | 0001          |
-      | 0005           | 2021-03-23 18:29:44  | 28934       | 3266     | 3,6               | 0001          |
+      | reservation_id | reservation_date| reservation_time | customer_name | table_id | table_coordinates | restaurant_id |
+      | 0001           | 2021-03-23      | 20:34:45         | adam        | 1        | 0,2               | 0001          |
+      | 0002           | 2021-03-23      | 19:48:58         | tom        | 2        | 0,8               | 0001          |
+
     When <restaurant_owner> queries reservations with <reservation_datetime> ranging from 2021-03-23 19:00:00 to 2021-03-23 20:00:00 in <restaurant_id>
     Then the following list of all tables in <restaurant_id> is returned:
-      | reservation_id | customer_id | table_id | table_capacity | table_reserved | table_coordinates |
-      | 0002           | 12234       | 3240     | 4              | true           | 0,8               |
-      | 0003           | 82903       | 2358     | 2              | true           | 2,6               |
-      | null           | null        | 3246     | 3              | false          | 0,2               |
-      | null           | null        | 3266     | 2              | false          | 3,6               |
-      | null           | null        | 3333     | 5              | false          | 3,3               |
+      | reservation_id | customer_name | table_id | table_capacity | table_coordinates |
+      | 0002           | tom           | 2        | 5              | 0,8               |
--- bmt-backend/src/test/java/bookmytable/stepdefinitions/ViewMapOfReservations.java
+++ bmt-backend/src/test/java/bookmytable/stepdefinitions/ViewMapOfReservations.java
@@ -1,48 +1,136 @@
 package bookmytable.stepdefinitions;
 
+import bookmytable.dao.CustomerRepository;
+import bookmytable.dao.ReservationRepository;
+import bookmytable.dao.RestaurantOwnerRepository;
+import bookmytable.dao.RestaurantRepository;
+import bookmytable.model.*;
+import bookmytable.service.CustomerRegistrationService;
+import bookmytable.service.ReservationService;
+import bookmytable.service.RestaurantService;
+import bookmytable.service.ViewReservationMapService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ViewMapOfReservations {
+
+	@Autowired
+	private ViewReservationMapService serviceVR;
+	@Autowired
+	private RestaurantService serviceR;
+	@Autowired
+	private RestaurantRepository rRepo;
+	@Autowired
+	private CustomerRepository cRepo;
+	@Autowired
+	private ReservationRepository resvRepo;
+	@Autowired
+	private RestaurantOwnerRepository oRepo;
+	@Autowired
+	private ReservationService serviceRes;
+	@Autowired
+	private CustomerRegistrationService serviceC;
+
+
+	private RestaurantOwner owner;
+	private Restaurant restaurant;
+	private Set<Restaurant> restaurants = new HashSet<>();
+	private String[] customerEmails = {"john12345@gmail.com", "pizzaman@hotmail.com"};
+	private String customerPassword = "password123";
+	private String customerPhoneNumber = "123-456-7890";
+	private List<Reservation> reservations = null;
+
 	@Given("restaurant owner <restaurant_owner> is logged into BookMyTable as a restaurant owner")
 	public void restaurant_owner_restaurant_owner_is_logged_into_book_my_table_as_a_restaurant_owner() {
-		// Write code here that turns the phrase above into concrete actions
+		owner = new RestaurantOwner();
+		owner.setName("test");
+		owner.setEmail("test@gmail.com");
+		owner.setPassword("testPassword");
+		owner.setRestaurants(restaurants);
+		oRepo.save(owner);
 	}
 
 	@Given("<restaurant_owner> owns <restaurant_id>")
 	public void restaurant_owner_owns_restaurant_id() {
-		// Write code here that turns the phrase above into concrete actions
+		int[][] hours = new int[4][2];
+		int estDuration = 180;
+		String menuLink = "aLink";
+		int price = 2;
+		String cuisine = "italian";
+		String options = "pizzatime";
+		String testName = "pizzeria";
+		String testAddress = "testAddress";
+		this.restaurant = serviceR.createRestaurant(testName, testAddress, hours, owner, estDuration, menuLink, price, cuisine, options);
+		restaurants.add(restaurant);
+		owner.setRestaurants(restaurants);
+		oRepo.save(owner);
 	}
 
 	@Given("the following reservations are registered to the restaurant <restaurant_id>:")
 	public void the_following_reservations_are_registered_to_the_restaurant_restaurant_id(
-			io.cucumber.datatable.DataTable dataTable) {
-		// Write code here that turns the phrase above into concrete actions
-		// For automatic transformation, change DataTable to one of
-		// E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-		// Double, Byte, Short, Long, BigInteger or BigDecimal.
-		//
-		// For other transformations you can register a DataTableType.
+			io.cucumber.datatable.DataTable dataTable) throws ParseException {
+		List<Map<String, String>> valueMaps = dataTable.asMaps();
+		int i =0;
+		for (Map<String, String> map : valueMaps) {
+			java.util.Date res_date = new SimpleDateFormat("yyyy-MM-dd").parse(map.get("reservation_date"));
+			Date dateFormat = new Date(res_date.getTime());
+			DateFormat formatter = new SimpleDateFormat("kk:mm:ss");
+			Time time = new Time(formatter.parse(map.get("reservation_time")).getTime());
+			Integer res_ID = Integer.decode(map.get("reservation_id"));
+			Integer table_ID = Integer.decode(map.get("table_id"));
+			int x_coord = Integer.parseInt(map.get("table_coordinates").substring(0,1));
+			int y_coord = Integer.parseInt(map.get("table_coordinates").substring(2,3));
+			serviceR.addTableToMap(5, table_ID, x_coord, y_coord, restaurant);
+			RestaurantTable table = new RestaurantTable();
+			table.setId((long)table_ID);
+			table.setX(x_coord);
+			table.setY(y_coord);
+			table.setCapacity(5);
+			table.setRestaurant(restaurant);
+			table.setTableNumber(table_ID);
+			Customer customer = serviceC.createCustomer(map.get("customer_name"), customerEmails[i], customerPassword, customerPhoneNumber);
+			serviceRes.makeReservation(time, dateFormat, 1, res_ID, table, customer, restaurant);
+			i++;
+		}
 	}
 
 	@When("<restaurant_owner> queries reservations with <reservation_datetime> ranging from {int}-{int}-{int} {int}:{int}:{int} to {int}-{int}-{int} {int}:{int}:{int} in <restaurant_id>")
 	public void restaurant_owner_queries_reservations_with_reservation_datetime_ranging_from_to_in_restaurant_id(
 			Integer int1, Integer int2, Integer int3, Integer int4, Integer int5, Integer int6, Integer int7,
 			Integer int8, Integer int9, Integer int10, Integer int11, Integer int12) {
-		// Write code here that turns the phrase above into concrete actions
+		Date startDate = new Date(int1-1900, int2-1, int3);
+		Time startTime = new Time(int4, int5, int6);
+		Time endTime = new Time(int10, int11, int12);
+		reservations = serviceRes.getReservationsByRestaurantAndStartTimeBetweenAndDate(restaurant, startTime, endTime, startDate);
 	}
 
 	@Then("the following list of all tables in <restaurant_id> is returned:")
 	public void the_following_list_of_all_tables_in_restaurant_id_is_returned(
-			io.cucumber.datatable.DataTable dataTable) {
-		// Write code here that turns the phrase above into concrete actions
-		// For automatic transformation, change DataTable to one of
-		// E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-		// Double, Byte, Short, Long, BigInteger or BigDecimal.
-		//
-		// For other transformations you can register a DataTableType.
+			io.cucumber.datatable.DataTable dataTable) throws ParseException {
+		List<Map<String, String>> valueMaps = dataTable.asMaps();
+		int i = 0;
+		for (Map<String, String> map : valueMaps) {
+			assertEquals(reservations.get(i).getId(), Integer.parseInt(map.get("reservation_id")));
+			assertEquals(reservations.get(i).getCustomer().getName(), map.get("customer_name"));
+			assertEquals(reservations.get(i).getTable().getId(), Integer.parseInt(map.get("table_id")));
+			assertEquals(reservations.get(i).getTable().getCapacity(), Integer.parseInt(map.get("table_capacity")));
+			assertEquals(reservations.get(i).getTable().getX(), Integer.parseInt(map.get("table_coordinates").substring(0,1)));
+			assertEquals(reservations.get(i).getTable().getY(), Integer.parseInt(map.get("table_coordinates").substring(2,3)));
+			i++;
+		}
 	}
 }
