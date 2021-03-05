94583628c5880ee3d2cb17d6eaf2333080bd4515
yuterzbell <yuta.bellali@mail.mcgill.ca>
2021-03-05 16:20:47 -0500

    restaurant modification UI design

---
+++ bmt-frontend/src/components/ModifyOpeningHours.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="page">
+
+  <center class="textStyle">Modify Opening Hours</center>
+  <input type="text">
+  <div class="row">
+  <button type="submit" class="button">Submit modification</button>
+  </div>
+
+</div>
+</template>
+
+<style>
+.button{
+  background-color: #e7e7e7; /* Green */
+   border: none;
+   color: black;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   font-size: 16px;
+   cursor: pointer;
+   margin-top: 50px;
+   margin-left: auto;
+   margin-right: auto;
+   width:300px;
+   height:50px
+
+}
+
+.page{
+  text-align: center;
+  padding-top: 25px;
+  float: center;
+}
+.field{
+  padding-top: 25px;
+}
+
+.textStyle{
+font-size: 30px;
+padding-bottom: 100px;
+}
+</style>
+
+<script>
+
+</script>
---
+++ bmt-frontend/src/components/ModifyRestaurantSelector.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="page">
+
+  <center class="textStyle">What would you like to modify?</center>
+
+    <div class="row">
+    <button type="submit" class="button">Modify Food Options</button>
+    </div>
+    <div class="row">
+    <button type="submit" class="button">Modify Location</button>
+    </div>
+    <div class="row">
+    <button type="submit" class="button">Modify Menu</button>
+    </div>
+    <div class="row">
+    <button type="submit" class="button">Modify Restaurant Name</button>
+    </div>
+    <div class="row">
+    <button type="submit" class="button">Modify Opening Hours</button>
+    </div>
+    <div class="row">
+    <button type="submit" class="button">Modify Price Category</button>
+    </div>
+    <div class="row">
+    <button type="submit" class="button">Modify Food Type</button>
+    </div>
+
+</div>
+</template>
+
+<style>
+.button{
+  background-color: #e7e7e7; /* Green */
+   border: none;
+   color: black;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   font-size: 16px;
+   cursor: pointer;
+   margin-top: 50px;
+   margin-left: auto;
+   margin-right: auto;
+   width:300px;
+   height:50px
+
+}
+
+.page{
+  padding-top: 25px;
+  float: center;
+}
+.field{
+  padding-top: 25px;
+}
+
+.textStyle{
+font-size: 30px;
+}
+</style>
+
+<script>
+
+</script>
