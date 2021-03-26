c564624bf9503c2369233f571c19ca168e987821
Antonia Nistor <antonianistor@hotmail.fr>
2021-03-26 17:12:03 -0400

    ui modify reservation

---
+++ bmt-frontend/src/components/ModifyReservation.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="row">
+<div class="column">
+  <h3 onload="maximum()">Modify Your Reservation</h3>
+  <form>
+  <form>
+    <label class="field" for="quantity">Group Size </label>
+    <input type="number" id="mod-quantity" name="mod-quantity" min="1" max="10" v-model="quantity">
+    </form>
+    <form>
+  <label class="field" for="date">Date:</label>
+  <input type="date" id="mod-date" name="mod-date" v-model="date">
+  </form>
+  <form>
+  <label class="field" for="appt">Time</label>
+  <input type="time" id="mod-appt" name="mod-appt" min="09:00" max="18:00" v-model="appt" >
+  </form>
+  <form>
+  <label class="field" for="table">Table</label>
+  <select id="mod-table" name="mod-table" v-model='table'>
+     <option v-for="table in tables" v-bind:value="{ id: table.id }" :key="table.tableNumber"> {{table.tableNumber}}</option>
+  </select>
+</form>
+    <button type="submit" class="button" >Modify Reservation</button>
+    </form>
+
+</div>
+<div class="column">
+  <h3>Restaurant's table layout</h3>
+  <img src="" alt="Insert restaurant's table layout">
+</div>
+</div>
+</template>
+
+<style>
+.button{
+  background-color: #e7e7e7; /* Green */
+   border: none;
+   color: black;
+   padding: 15px 32px;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   font-size: 16px;
+   cursor: pointer;
+   margin-top: 50px;
+}
+
+.column{
+  padding-top: 25px;
+  padding-left: 200px;
+  float: center;
+  width: 50%;
+}
+.field{
+  padding-top: 25px;
+}
+</style>
+
+<script>
+
+</script>
